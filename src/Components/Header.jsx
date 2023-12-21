3ed33682a1e49d50042eccf30fcf38576a1d82e4
Sushank Gautam <sushankg4@gmail.com>
2023-12-21 23:01:09 +0530

    added

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>To-Do Application</h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Components/Home.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import '../styles/Home.css';
+import { MdDelete } from "react-icons/md";
+import { FaRegEdit } from "react-icons/fa";
+
+const Home = () => {
+    const [task, setTask] = useState('');
+    const [tasks, setTasks] = useState([]);
+    const handleText = (e) => {
+        setTask(e.target.value);
+    };
+
+
+    const handleTask = () => {
+        if (task === '') {
+            alert('Please enter any task!');
+        } else {
+            const currentTime = new Date();
+            const taskWithTime = {
+                text: task,
+                time: currentTime.toLocaleString()
+            };
+
+            setTasks([...tasks, taskWithTime]);
+            setTask('');
+        }
+    };
+
+    const handleDelete = (index) => {
+        const updatedTasks = [...tasks];
+        updatedTasks.splice(index, 1);
+        setTasks(updatedTasks);
+    };
+
+    return (
+        <div className='home-component'>
+            <div className='home-main'>
+                <div className='input-div'>
+                    <input type='text' placeholder='Enter your task' onChange={handleText} value={task} />
+                    <button onClick={handleTask}>Add</button>
+                </div>
+
+                <div className='tasks-div'>
+                    {tasks.map((taskItem, index) => (
+                        <div key={index} className='task'>
+                            <h2>{taskItem.text}</h2>
+                            <p>{taskItem.time}</p>
+                            <div className='task-right'>
+                                <FaRegEdit cursor={'pointer'} />
+                                <MdDelete cursor={'pointer'} onClick={() => handleDelete(index)} />
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+            </div>
+
+            <h1>THIS SITE IS UNDER CONSTRUCTION</h1>
+        </div>
+    );
+};
+
+export default Home;
