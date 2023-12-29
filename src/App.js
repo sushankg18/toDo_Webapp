668ee8b73882ec0daa7f872311982b0541901b6e
Sushank Gautam <sushankg4@gmail.com>
2023-12-29 16:02:14 +0530

    added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./Components/Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
+import Footer from "./Components/Footer";
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer-main'>
+        <div>
+            <h3>all right reserved by @todo_Webapp</h3>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
